e06196ada7b503c14304fac6da4635771d1b2c1d
Alexandr Gerasimov <phantom6391@yandex.ru>
2023-11-20 21:46:14 +0400

    Правки апи

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.aeg.nomoredomainsmonster.ru/');
+  const api = new Api('https://api.aeg.nomoredomainsmonster.ru');
 
   export default api;
